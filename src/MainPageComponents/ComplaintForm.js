ed269685d7ce09c9733a72a2af9a18b9ca00c36b
codecrusher15 <pushkarpawar2002@gmail.com>
2022-02-12 00:22:30 +0530

    complaint form updated

--- src/MainPageComponents/ComplaintForm.js
+++ src/MainPageComponents/ComplaintForm.js
@@ -6,61 +6,46 @@ export default function ComplaintForm() {
 	
 	<h1>Complaint Form</h1> <br/>
 	
-	<form class="w-full max-w-lg h-full">
+	<form class="w-full max-w-lg h-full text-xl">
 		<div class="flex flex-wrap -mx-3 mb-6">
-			<div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-				<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-					First Name
+			<div class="w-full md:w-full px-3 mb-6 md:mb-0">
+				<label class="block uppercase tracking-wide text-gray-700 font-bold mb-2" for="grid-first-name">
+					Subject
 				</label>
 			<input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane"/>
 			<p class="text-red-500 text-xs italic">Please fill out this field.</p>
-			</div>
-
-			<div class="w-full md:w-1/2 px-3">
-				<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-					Last Name
-				</label>
-				<input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe"/>
-			</div>
 		</div>
-	<div class="flex flex-wrap -mx-3 mb-6">
-		<div class="w-full px-3">
-		<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-			Password
-		</label>
-		<input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************"/>
-		<p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
 		</div>
-	</div>
-		<div class="flex flex-wrap -mx-3 mb-2">
-		<div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-		<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-			City
-		</label>
-		<input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Albuquerque"/>
+		<div class="flex flex-wrap mb-6">
+			<label for="exampleFormControlTextarea1" class="form-label inline-block mb-2 text-gray-700"
+			>Enter Complaint</label>
+			<textarea
+			class="
+				form-control
+				block
+				w-full
+				px-3
+				py-1.5
+				text-base
+				font-normal
+				bg-clip-padding
+				border border-solid border-gray-300
+				rounded
+				transition
+				ease-in-out
+				m-0
+				focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+				bg-gray-200 text-gray-700
+			"
+			id="exampleFormControlTextarea1"
+			rows="3"
+			placeholder="Your message"
+			></textarea>
 		</div>
-	<div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-	<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-		State
-	</label>
-	<div class="relative">
-		<select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-		<option>New Mexico</option>
-		<option>Missouri</option>
-		<option>Texas</option>
-		</select>
-		<div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-		<svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+		<br/>
+		<div class="flex space-x-2 justify-center">
+  			<button type="button" class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Register Compliant</button>
 		</div>
-	</div>
-	</div>
-	<div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-	<label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-		Zip
-	</label>
-	<input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="90210"/>
-	</div>
-	</div>
 	</form>
 
 	</div>
